7e77f2ff23a525210fa806df2985c2a8ab12ebda
dyushakolesov <dyushakolesov@mail.ru>
2019-09-21 16:20:03 +0300

    fixed; console_demo

--- shoot_the_bullet_vs/GameField.h
+++ shoot_the_bullet_vs/GameField.h
@@ -18,5 +18,25 @@ public:
 private:
 	int score;
 	int hiscore;
-	int game_field[LCD_WIDTH][LCD_HEIGHT];
+	int game_field[LCD_HEIGHT][LCD_WIDTH];
+};
+
+class Player :public GameField {
+public:
+	GameField field;
+	Player();
+	Player(GameField& field);
+	Player(int w, int h);
+	void moveLeft();
+	void moveRight();
+	void moveUp();
+	void moveDown();
+	void shoot();
+	int  getWeight() { return width; }
+	int getHeight() { return height; }
+	int SetHeight(int h) { h = height; }
+	int SetWidth(int h) { h = width; }
+private:
+	int width;
+	int height;
 };
--- shoot_the_bullet_vs/main.cpp
+++ shoot_the_bullet_vs/main.cpp
@@ -1,28 +1,15 @@
 #include "GameField.h"
-#include "Player.h"
-#include  <iostream>
 #include "windows.h"
-
+#include <iostream>
 //..
 const int LCD_WIDTH = 84;
 const int LCD_HEIGHT = 48;
-void GameField::PrintField()
-{
-	for (int i = 0; i < LCD_WIDTH;i++) {
-		{
-			for (int j = 0; j < LCD_HEIGHT;j++)
-				if(game_field[i][j] == 0)std::cout << "  ";
-				else if(game_field[i][j] == 1)std::cout << "[]";
-				else if (game_field[i][j] == 2)std::cout << "{}";
-				else if (game_field[i][j] == 3)std::cout << "<>";
-		}
-	}
-}
-void GameField::PrintDestructBlock(int width, int height)
+
+void PrintDestructBlock(int width, int height)
 {
 	std::cout << "{}";
 }
-void GameField::PrintUnDestructBlock(int width, int height)
+void PrintUnDestructBlock(int width, int height)
 {
 	std::cout << "[]";
 }
@@ -30,8 +17,7 @@ void GameField::PrintUnDestructBlock(int width, int height)
 
 
 int main() {
-	GameField field;
-	Player player(field);
+	Player player;
 	int i = 0;
 	char input;
 	std::cout << " press w to move up;" << std::endl;
@@ -40,31 +26,34 @@ int main() {
 	std::cout << " press d to move right;" << std::endl;
 	std::cout << " press q to move quit;" << std::endl;
 	std::cout << " press f to shoot;" << std::endl;
+	while (!player.field.isGameOver) {
 
-	while (!field.isGameOver) {
-
-		field.CreateNewLine(i);
+		player.field.CreateNewLine(i);
 		for (int action_idx = 0; action_idx < 10; action_idx++) {
-			field.PrintField();
-			std::cin >> input;
+			if (!player.field.isGameOver) {
+				player.field.PrintField();
+				std::cin >> input;
 
-			if (input == 'w') player.moveUp(field);
-			else if (input == 'a') player.moveLeft(field);
-			else if (input == 's') player.moveDown(field);
-			else if (input == 'd') player.moveRight(field);
-			else if (input == 'f') player.shoot(field);
-			else std::cout << "incorrect input!";
-			if (!field.isGameOver) {
-				std::cout << "Game over! your score: "
-					<< field.getScore() << std::endl;
+				if (input == 'w') player.moveUp();
+				else if (input == 'a') player.moveLeft();
+				else if (input == 's') player.moveDown();
+				else if (input == 'd') player.moveRight();
+				else if (input == 'f') player.shoot();
+				else std::cout << "incorrect input!";
+				if (player.field.isGameOver) {
+					std::cout << "Game over! your score: "
+						<< player.field.getScore() << std::endl;
+					std::cout << "Press any key to exit: " << std::endl;
+					system("pause");
+				}
 			}
 		}
 		
 		//print player
-		field.PrintField();
+		player.field.PrintField();
 
 		i++;
-		if (i >= GameField::LCD_WIDTH)i = 0;
+		if (i>=LCD_WIDTH)i = 0;
 
 	}
 	return 0;
