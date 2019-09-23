b9f0c1866fe2f3a213a98ee30aa9cf645ee11d33
dyushakolesov <dyushakolesov@mail.ru>
2019-09-23 21:34:46 +0300

    fix?

--- shoot_the_bullet_vs/shoot_the_bullet.cpp
+++ shoot_the_bullet_vs/shoot_the_bullet.cpp
@@ -26,28 +26,24 @@ int main() {
 	std::cout << " press a to move right;" << std::endl;
 	std::cout << " press s to move left;" << std::endl;
 	std::cout << " press d to move right;" << std::endl;
-	std::cout << " press q to move quit;" << std::endl;
 	std::cout << " press f to shoot;" << std::endl;
 	std::cout << "******************" << std::endl;
 	std::cout << "[] - undestruct block" << std::endl;
 	std::cout << "{} - destruct block" << std::endl;
 	std::cout << "<> - player" << std::endl;
-	system("pause");
 
 	while (!player.field.isGameOver) {
 
 		player.field.CreateNewLine(i);
 		for (int action_idx = 0; action_idx < 10; action_idx++) {
-			if (!player.field.isGameOver) {
-				player.field.PrintField();
-				input =_getch();
-
+			if (!player.field.isGameOver) {	
+				player.field.PrintField();		
+				input = _getch();
 				if (input == 'w') player.moveUp();
 				else if (input == 'a') player.moveLeft();
 				else if (input == 's') player.moveDown();
 				else if (input == 'd') player.moveRight();
 				else if (input == 'f') player.shoot();
-				else std::cout << "incorrect input!";
 				if (player.field.isGameOver) {
 					std::cout << "Game over! your score: "
 						<< player.field.getScore() << std::endl;
